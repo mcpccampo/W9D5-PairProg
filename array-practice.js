8288cc521f776d8326cdcce98739e8a5b0ad4a9d
Camilo Campo <mcp.ccampo@gmail.com>
2022-05-21 16:20:37 -0400

    array practice #2

--- array-practice.js
+++ array-practice.js
@@ -1,39 +1,53 @@
-const findMinimum = arr => {
-
-  // Your code here
-
+const findMinimum = (arr) => {
+  let min = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
 };
 
-const runningSum = arr => {
+const runningSum = (arr) => {
+  let sumArr = [];
+  if (!arr.length) {
+    return [];
+  }
 
+  let arrRes = arr.reduce((acc, el) => {
+    let total = acc + el;
+    sumArr.push(total);
+    return acc + el;
+  }, 0);
+  return sumArr;
+};
+
+const evenNumOfChars = (arr) => {
   // Your code here
 };
 
-const evenNumOfChars = arr => {
-
+const smallerThanCurr = (arr) => {
   // Your code here
-};
-
-const smallerThanCurr = arr => {
-
-  // Your code here
-
 };
 
 const twoSum = (arr, target) => {
-
   // Your code here
 };
 
-const secondLargest = arr => {
-
+const secondLargest = (arr) => {
   // Your code here
 };
 
 const shuffle = (arr) => {
-
   // Your code here
 };
 
-
-module.exports = [findMinimum, runningSum, evenNumOfChars, smallerThanCurr, twoSum, secondLargest, shuffle];
+module.exports = [
+  findMinimum,
+  runningSum,
+  evenNumOfChars,
+  smallerThanCurr,
+  twoSum,
+  secondLargest,
+  shuffle,
+];
